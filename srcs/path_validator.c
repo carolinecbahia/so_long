fa6a8a58549e3655aa0f824b2f5383c76380e979
Caroline Bahia <carolinecbahia@gmail.com>
2025-11-25 15:50:45 -0300

    11-24 update athome

--- srcs/path_validator.c
+++ srcs/path_validator.c
@@ -3,31 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   path_validator.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ccavalca <ccavalca@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ccavalca <ccavalca@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/10 20:44:05 by ccavalca          #+#    #+#             */
-/*   Updated: 2025/10/30 15:30:59 by ccavalca         ###   ########.fr       */
+/*   Updated: 2025/11/23 23:28:21 by ccavalca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-static void	flood_fill(char **map, int x, int y)
+void	flood_fill(char **map, int x, int y, int allow_exit)
 {
-	if (x < 0 || y < 0 || !map[y] || !map[y][x] || WALL)
+	if (x < 0 || y < 0 || !map[y] || !map[y][x])
 		return ;
 	if (map[y][x] == VISITED)
 		return ;
-	if ((map[y][x] == COLLECTIBLE || map[y][x] == EXIT
-			|| map[y][x] == EMPTY || map[y][x] == PLAYER))
+	/* mark walkable tiles as visited so we don't recurse forever */
+	if (map[y][x] == COLLECTIBLE || map[y][x] == EMPTY ||
+		map[y][x] == EXIT || map[y][x] == PLAYER)
 		map[y][x] = VISITED;
-	flood_fill(map, x + 1, y);
-	flood_fill(map, x - 1, y);
-	flood_fill(map, x, y + 1);
-	flood_fill(map, x, y - 1);
+	flood_fill(map, x + 1, y, allow_exit);
+	flood_fill(map, x - 1, y, allow_exit);
+	flood_fill(map, x, y + 1, allow_exit);
+	flood_fill(map, x, y - 1, allow_exit);
 }
 
-static int	check_remaining(char **map_copy)
+int	check_remaining(char **map_copy)
 {
 	int	y;
 	int	x;
@@ -47,17 +48,45 @@ static int	check_remaining(char **map_copy)
 	return (0);
 }
 
+int	check_collectibles_remaining(char **map_copy)
+{
+	int	y;
+	int	x;
+
+	y = 0;
+	while (map_copy[y])
+	{
+		x = 0;
+		while (map_copy[y][x])
+		{
+			if (map_copy[y][x] == COLLECTIBLE)
+				return (-1);
+			x++;
+		}
+		y++;
+	}
+	return (0);
+}
+
 int	path_validator(t_game *map_data)
 {
 	char		**map_copy;
 	t_vectors	player_pos;
 
+	find_player_pos(map_data, &player_pos);
 	map_copy = dup_matrix(map_data->matrix);
 	if (!map_copy)
 		return (-1);
-	find_player_pos(map_data, &player_pos);
-	flood_fill(map_copy, player_pos.x, player_pos.y);
-	if (check_remaining(map_copy) == 0)
+	flood_fill(map_copy, player_pos.x, player_pos.y, 0);
+	if (check_collectibles_remaining(map_copy) != 0)
+	{
+		ft_free_matrix(map_copy);
+		return (-1);
+	}
+	ft_free_matrix(map_copy);
+	map_copy = dup_matrix(map_data->matrix);
+	flood_fill(map_copy, player_pos.x, player_pos.y, 1);
+	if (check_remaining(map_copy) != 0)
 	{
 		ft_free_matrix(map_copy);
 		return (-1);
